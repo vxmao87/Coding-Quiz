f3a68b9caa6abee97bb8bf11653c795d20e9b899
Vinh Mao <vinhxmao87@gmail.com>
2020-03-28 21:50:32 -0700

    3rd commit, js code and CSS code set up

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # homework-4
+
+This is the 4th homework of the UW Coding Bootcamp, which requires me to create a quiz show using JavaScript. It must ask questions about our current class and cycle through every question once answered until the timer reaches zero or all questions have been answered. Then we need to use local storage to save high scores and name.
+
+I had a lot of trouble doing this assignment, therefore it is mostly incomplete aside from establishing a timer and the buttons needed to display the potential answers.
--- script.js
+++ script.js
@@ -39,34 +39,78 @@ var questionEl = document.querySelector(".question");
 var starterEl = document.querySelector("#starter");
 var answerEl = document.querySelector(".answers");
 var timeEl = document.querySelector("#time");
-var score = 0;
+var score = localStorage.getItem("score");
 var timeLeft = 60;
 var questionNumber = 0;
+var name = localStorage.getItem("name");
 
 starterEl.addEventListener("click", function() {
-    console.log("clicked!");
-    var timerInterval = setInterval(function() {
-        timeLeft--;
-        timeEl.textContent = timeLeft;
-
-        if(timeLeft == 0) {
-            clearInterval(timerInterval);
-        }
-
-    }, 1000);
-
     questionEl.textContent = questions[questionNumber].q;
     var buttonDiv1 = document.createElement("button");
     var buttonDiv2 = document.createElement("button");
     var buttonDiv3 = document.createElement("button");
     var buttonDiv4 = document.createElement("button");
-    buttonDiv1.textContent = questions[questionNumber].a1;
-    buttonDiv2.textContent = questions[questionNumber].a2;
-    buttonDiv3.textContent = questions[questionNumber].a3;
-    buttonDiv4.textContent = questions[questionNumber].a4;
+    var a1Correct = false;
+    var a2Correct = false;
+    var a3Correct = false;
+    var a4Correct = false;
+    buttonDiv1.setAttribute("id", "a1");
+    buttonDiv2.setAttribute("id", "a2");
+    buttonDiv3.setAttribute("id", "a3");
+    buttonDiv4.setAttribute("id", "a4");
     answerEl.appendChild(buttonDiv1);
     answerEl.appendChild(buttonDiv2);
     answerEl.appendChild(buttonDiv3);
     answerEl.appendChild(buttonDiv4);
 
+    function generateQuestion(questionNumber) {
+        buttonDiv1.textContent = questions[questionNumber].a1;
+        buttonDiv2.textContent = questions[questionNumber].a2;
+        buttonDiv3.textContent = questions[questionNumber].a3;
+        buttonDiv4.textContent = questions[questionNumber].a4;
+
+        for(var i = 1; i <= 4; i++) {
+            var correctAnswer = questions[questionNumber].correct;
+            if(correctAnswer == "a1") {
+                a1Correct = true;
+            } else if(correctAnswer == "a2") {
+                a2Correct = true;
+            } else if(correctAnswer == "a3") {
+                a3Correct = true;
+            } else if(correctAnswer == "a4") {
+                a4Correct = true;
+            }
+        }
+    }
+    generateQuestion(questionNumber);
+
+    function setScoreBoard() {
+        localStorage.setItem("score", score);
+        questionEl.textContent = name + " " + score;
+    }
+
+    function isCorrect(correctAnswer) {
+        if(correctAnswer) {
+            score++;
+        } else {
+            timeLeft-= 2;
+        }
+        questionNumber++;
+        generateQuestion(questionNumber);
+    }
+    var timerInterval = setInterval(function() {
+        timeLeft--;
+        timeEl.textContent = timeLeft;
+
+        if(timeLeft === 0 || questionNumber == questions.length - 1) {
+            clearInterval(timerInterval);
+            setScoreBoard();
+        }
+
+    }, 1000);
+
+    buttonDiv1.addEventListener("click", isCorrect(a1Correct));
+    buttonDiv2.addEventListener("click", isCorrect(a2Correct));
+    buttonDiv3.addEventListener("click", isCorrect(a3Correct));
+    buttonDiv4.addEventListener("click", isCorrect(a4Correct));
 });
